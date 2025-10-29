f7fb1287fee6d2e6c6341f3be682956e87fec0f6
Gabriel Martin <gm9984707@gmail.com>
2025-10-29 13:49:02 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from manager import Manager
 
-if __name__=="__main__":   #desded aca es donde se ejecuta el script, INDEX
+if __name__=="__main__":   #desde aca es donde se ejecuta el script, INDEX
     app = Manager()
+
     app.mainloop()
